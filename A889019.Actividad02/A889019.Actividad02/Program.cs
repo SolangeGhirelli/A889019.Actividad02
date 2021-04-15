2b8a1d54eb9ae388055361d4504871c380cfa5eb
SOLANGE GHIRELLI <17GH40346082@campus.economicas.uba.ar>
2021-04-15 18:27:29 -0300

    Punto 1

---
+++ A889019.Actividad02/A889019.Actividad02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A889019.Actividad02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int CantidadDeEquipos;
+
+            Console.WriteLine("Ingrese cantidad de equipos: ");
+            string ingreso = Console.ReadLine();
+            bool ValidarIngreso = Int32.TryParse(ingreso, out CantidadDeEquipos);
+
+            do
+            {
+                if (!ValidarIngreso)
+                {
+                    Console.WriteLine("Ingreso incorrecto");
+                    break;
+                }
+                if (CantidadDeEquipos < 2)
+                {
+                    Console.WriteLine("Ingrese más de dos equipos");
+                    ingreso = Console.ReadLine();
+                    ValidarIngreso = Int32.TryParse(ingreso, out CantidadDeEquipos);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Ingreso {CantidadDeEquipos} equipos.");
+                    break;
+                }
+
+            } while (ValidarIngreso);
+
+            List<int> NumeroDeEquipos = new List<int>(CantidadDeEquipos);
+
+            for (int indice = 1; indice <= CantidadDeEquipos; indice++)
+            {
+                    Console.WriteLine($"Indique un nombre para {indice}: ");
+                    string ingreso2 = Console.ReadLine();                    
+            }
+
+            Dictionary<int, string> Equipos = new Dictionary<int, string>();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
